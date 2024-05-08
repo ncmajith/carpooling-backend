b6a99147336392171acfe9a8ffea2c12660621b1
ncmajith <mywldthebest@gmail.com>
2024-05-08 18:27:34 +0530

    Initial commit

---
+++ src/main/java/dev/carpooling/carpooingbackend/CarpooingBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.carpooling.carpooingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpooingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpooingBackendApplication.class, args);
+	}
+
+}
